2b44900968787888fbb16724d74d75ce05b06849
YismaelDiazH <105748001+YismaelDiazH@users.noreply.github.com>
2024-03-18 22:28:51 +0100

    dotenv fixed

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-require("dotenv").config();
-
+import { config } from "dotenv";
 import express from "express";
 import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 import multer from "multer";
+
 const app = express();
+config();
 
 app.use(express.json());
 app.use(cookieParser());
