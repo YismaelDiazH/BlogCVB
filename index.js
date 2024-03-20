9691619fb49225b9fb7cebdd8f8675e3dc50c6f6
YismaelDiazH <105748001+YismaelDiazH@users.noreply.github.com>
2024-03-20 15:01:43 +0100

    intento 10000

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
           
 
 app.listen(PORT, () => {
