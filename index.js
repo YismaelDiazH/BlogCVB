1c163798d792e24702ddf7a8f4a498fc6cebdc9b
YismaelDiazH <105748001+YismaelDiazH@users.noreply.github.com>
2024-03-20 09:55:57 +0100

    cambio de port desarrollo

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
           
 
 app.listen(PORT, () => {
